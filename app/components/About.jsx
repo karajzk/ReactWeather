f9c066e0f6064bd5dbfe0daceaada485b73839fc
Karajz Károly <karajzk@hotmail.com>
2017-04-11 11:29:30 +0200

    css

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 
 var About = () => (
-    <h1 className="text-center">About component</h1>
+    <h1 className="text-center page-title">About component</h1>
   );
 
 module.exports = About;
--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,7 @@ module.exports = {
       WeatherMessage: 'app/components/WeatherMessage.jsx',
       openWeatherMap: 'app/api/openWeatherMap.jsx',
       ErrorModal: 'app/components/ErrorModel.jsx',
+      applicationStyles: 'app/styles/app.css',
     },
     extensions: ["",".js",".jsx"]
   },
